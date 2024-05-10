c1e715b8010884e395e06ab8787429aea22a383e
Nilushanth Thiruchelvam <Nilu@nilus-imac.home>
2024-05-10 11:58:13 -0400

    initial commit

---
+++ nilu-personal-website/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Nilushanth Thiruchelvam</h1>
+      <h3> Hi, I'm Nilushanth 👋 (you can call me Nilu)</h3>
+      
+
+
+    </div>
+  );
+}
+
+export default App;
